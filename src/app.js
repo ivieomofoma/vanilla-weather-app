ddc9973c6fb640371bb30c1bb4a895e45ad1f7a0
ivieomofoma <ivie.omofoma@gmail.com>
2023-09-24 09:27:51 +0100

    final commit

---
+++ src/app.js
@@ -0,0 +1,119 @@
+function formatTime(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let mintues = date.getMinutes();
+    if (mintues < 10) {
+      mintues = `0${mintues}`;
+    }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day}, ${hours}:${mintues}`;
+  }
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[day];
+  }
+  function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+  
+    forecast.forEach(function (forecastday, index) {
+      if (index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `<div class="col-2">
+                  <div class="weather-forecast-date">${formatDay(
+                    forecastday.dt
+                  )}</div>
+                  <img
+                    src="http:openweathermap.org/img/wn/${
+                      forecastday.weather[0].icon
+                    }@2x.png"
+                    alt=""
+                    width="50"
+                  />
+                  <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-max">${Math.round(
+                      forecastday.temp.max
+                    )}°</span>
+                    <span class="weather-forecast-min">${Math.round(
+                      forecastday.temp.min
+                    )}°</span>
+                  </div>
+                </div>
+    `;
+      }
+    });
+    forecastHTML = forecastHTML + `</div`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+  function getForecast(coordinates) {
+    let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+  }
+  
+  function getCurrentTemperature(response) {
+    let temperatureElement = document.querySelector("#temp");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElemnt = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#currentDate");
+    let iconElement = document.querySelector("#icon");
+  
+    celsiusTemp = response.data.main.temp;
+  
+    temperatureElement.innerHTML = Math.round(celsiusTemp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElemnt.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.wind.speed;
+    dateElement.innerHTML = formatTime(response.data.dt * 1000);
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute(
+      "alt",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+    );
+  
+    getForecast(response.data.coord);
+  }
+  
+  function search(city) {
+    let apiKey = "d7968ba5c334606078d89f51a7ec07e6";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+    axios.get(apiUrl).then(getCurrentTemperature);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#searchInput");
+    search(cityInputElement.value);
+  }
+  
+  let celsiusTemp = null;
+  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
+  
+  search("New York");
